271b9ce36b165f6d44fdb9a93e84bf10d00b83d8
nhan2708 <163325777+nhan2708@users.noreply.github.com>
2025-05-04 15:29:46 +0700

    Update app.py

--- app.py
+++ app.py
@@ -35,4 +35,7 @@ def send_message(psid, message):
     requests.post(url, json=payload)
 
 if __name__ == "__main__":
-    app.run(port=5000)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
